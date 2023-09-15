eca1c2b9da90d6aa24e1900ccc1a512fdb3c8168
Auth022 <60961366+mbongeb@users.noreply.github.com>
2023-09-15 15:25:48 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
     # Hands-on 2: replace <alias> with your alias, e.g. terraform-backend-dob-elgama
-    bucket = "terraform-backend-dob-<alias>"
+    bucket = "terraform-backend-dob-mbongeb"
     key    = "terraform-aws-project"
-    region = "eu-central-1"
+    region = "us-east-1"
   }
 }
